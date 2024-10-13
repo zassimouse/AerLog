b410ad1f8b5532a2db9efcc43ed491f31efd3fab
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2024-10-13 23:56:41 +0300

    Initial Commit

---
+++ AerLog/AerLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  AerLogApp.swift
+//  AerLog
+//
+//  Created by Denis Haritonenko on 13.10.24.
+//
+
+import SwiftUI
+
+@main
+struct AerLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
